af9bf62a67bfd416b3fa52bd1e7faf29b101cddf
andrewteece <andrewteece@gmail.com>
2024-11-18 17:50:55 -0600

    update layouyt and Header

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,23 @@
-import { Inter } from "next/font/google";
-import "@/assets/styles/globals.css";
+import { Inter } from 'next/font/google'
+import Header from '@/components/Header'
+import '@/assets/styles/globals.css'
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Bookit App | Book a Room",
-  description: "Book a meeting or conference room for your team",
-};
+  title: 'Bookit App | Book a Room',
+  description: 'Book a meeting or conference room for your team',
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={ inter.className }
-      >
-        <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        {children}
+    <html lang='en'>
+      <body className={inter.className}>
+        <Header />
+        <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
+          {children}
         </main>
-       
       </body>
     </html>
-  );
+  )
 }
- 
