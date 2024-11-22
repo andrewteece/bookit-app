c721c0df7688ff7bf6a84271178d021f4c1dc9bf
andrewteece <andrewteece@gmail.com>
2024-11-22 17:48:42 -0600

    html for Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,15 @@
 const Header = () => {
-  return <>Header</>
+  return (
+    <header className='bg-gray-100'>
+      <nav className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+        <div className='flex h-16 items-center justify-between'>
+          <div className='flex items-center'>
+            <img src='' alt='' />
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
